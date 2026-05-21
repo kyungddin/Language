aa64fc31c8a3c98eed23115bd60215a2dbb4fdfa
kyungmin.kim <kyungmin.kim@techwing.co.kr>
2026-05-21 16:59:37 +0900

    docs : upload wing dir

--- CSharp/hello_world/obj/Debug/net10.0/hello_world.AssemblyInfo.cs
+++ CSharp/hello_world/obj/Debug/net10.0/hello_world.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hello_world")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+debc88ccdae9fc1a0728ff895f586266a33cf93f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6e7fbf6ea056cad11086f2664debe5a38be5dc7")]
 [assembly: System.Reflection.AssemblyProductAttribute("hello_world")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hello_world")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
