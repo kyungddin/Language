41e616b13dafa5fc841d71d40eaed1950143a584
kyungddin <kyungddin@cau.ac.kr>
2026-05-31 13:59:29 +0900

    docs : add CString and Hungarian docs

---
+++ CPP/auto.md
@@ -0,0 +1,26 @@
+# C++ auto 자료형 정리
+
+
+## 참조를 놓치는 경우
+
+```cpp
+vector<int> v = {1,2,3};
+for (auto x : v) x = 0;   // (x) 복사본 수정, 원본 그대로
+for (auto& x : v) x = 0;  // (o) 참조로 받아야 원본 수정
+```
+
+
+## auto가 위험한 상황
+
+```cpp
+// 프록시 객체 - vector<bool> 의 함정
+// vector<bool>은 메모리 절약을 위해 비트 단위로 압축 저장하므로, 실제 bool과 좀 다름..
+vector<bool> v = {true, false};
+auto val = v[0];   // bool이 아니라 __bit_reference 라는 내부 타입으로 추론됨
+bool val = v[0];   // 이게 안전
+```
+
+## auto 권장사항
+- Google: 타입이 명확하거나 길면 auto 써라
+- 이터레이터, size_t, 람다 같은 특이 케이스에는 auto가 효과적
+- 임시 변수에도 auto가 효과적
--- CSharp/hello_world/obj/Debug/net10.0/hello_world.AssemblyInfo.cs
+++ CSharp/hello_world/obj/Debug/net10.0/hello_world.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hello_world")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6e7fbf6ea056cad11086f2664debe5a38be5dc7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09c5ec99bc303ab4b0b8cd26daa90d0696414937")]
 [assembly: System.Reflection.AssemblyProductAttribute("hello_world")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hello_world")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
